39f211d588df8102f3d1e634594b3a3e503151c1
Aleksandr <alexandr.falkone@yandex.ru>
2022-12-04 23:58:06 +0300

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -227,3 +227,102 @@ const lazyImagesObserver = new IntersectionObserver(loadImages, {
 });
 
 lazyImages.forEach((image) => lazyImagesObserver.observe(image));
+
+/*****************************************************************************************************************************************************/
+
+// Создание слайдера
+
+// Показать все слайдеры
+
+// const slider = document.querySelector(".slider");
+// slider.style.transform = "scale(0.4) translateX(1300px)";
+// slider.style.overflow = "visible";
+
+const sliders = document.querySelectorAll(".slide");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+const dotContainer = document.querySelector(".dots");
+
+let currentSlide = 0;
+const slidersNumber = sliders.length;
+
+// Добавляем точки для переключения слайдеров
+
+const createDots = function () {
+  sliders.forEach(function (_, index) {
+    dotContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="dots__dot" data-slide="${index}"></button>`
+    );
+  });
+};
+
+createDots();
+
+// Подсвечивание точки активного слайда
+
+const activeCurrentDot = function (slide) {
+  document
+    .querySelectorAll(".dots__dot")
+    .forEach((dot) => dot.classList.remove("dots__dot--active"));
+  document
+    .querySelector(`.dots__dot[data-slide='${slide}']`)
+    .classList.add("dots__dot--active");
+};
+
+activeCurrentDot(0);
+
+// Переключение слайдов
+
+const moveToSlide = function (slide) {
+  sliders.forEach(
+    (s, index) => (s.style.transform = `translateX(${(index - slide) * 100}%)`)
+  );
+};
+
+moveToSlide(0);
+
+// Переключение на следующий слайд
+
+const nextSlide = function () {
+  if (currentSlide === slidersNumber - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+  moveToSlide(currentSlide);
+  activeCurrentDot(currentSlide);
+};
+
+// Переключение на предыдущий слайд
+
+const previousSlide = function () {
+  if (currentSlide === 0) {
+    currentSlide = slidersNumber - 1;
+  } else {
+    currentSlide--;
+  }
+  moveToSlide(currentSlide);
+  activeCurrentDot(currentSlide);
+};
+
+btnRight.addEventListener("click", nextSlide);
+
+btnLeft.addEventListener("click", previousSlide);
+
+// Переключение слайдеров с помощью клавиатуры
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") nextSlide();
+  if (e.key === "ArrowLeft") previousSlide();
+});
+
+// Переключение слайдеров с помощью точки слайдера
+
+dotContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("dots__dot")) {
+    const slide = e.target.dataset.slide;
+    moveToSlide(slide);
+    activeCurrentDot(slide);
+  }
+});
